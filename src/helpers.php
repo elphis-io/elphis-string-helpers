57b8a8d333f1bb5c5958f47b45c5538470d15599
Saud Qureshi <saud.qureshi@vamaship.com>
2020-03-28 19:40:35 +0530

    Added string helpers

---
+++ src/helpers.php
@@ -0,0 +1,218 @@
+<?php
+
+namespace Elphis\Helpers\Str;
+
+/**
+ * check if two strings are equal.
+ *
+ * @param string $string1
+ * @param string $string2
+ * @param boolean $caseInsensitive
+ * @return boolean
+ */
+function str_equals(string $string1, string $string2,  bool $caseInsensitive = false)
+{
+    return ($caseInsensitive ? strcasecmp($string1, $string2) : strcmp($string1, $string2)) === 0;
+}
+
+/**
+ * check if the given $haystack string contains the $needle string
+ *
+ * @param string $haystack
+ * @param string $needle
+ * @param boolean $caseInsensitive
+ * @return boolean
+ */
+function str_contains(string $haystack, string $needle, bool $caseInsensitive = false)
+{
+    return ($caseInsensitive ? stripos($haystack, $needle) : strpos($haystack, $needle)) !== false;
+}
+
+/**
+ * check if given $haystack string starts with $needle string
+ *
+ * @param string $haystack
+ * @param string $needle
+ * @param boolean $caseInsensitive
+ * @return boolean
+ */
+function str_starts_with(string $haystack, string $needle, bool $caseInsensitive = false)
+{
+    return ($caseInsensitive ? stripos($haystack, $needle) : strpos($haystack, $needle)) === 0;
+}
+
+/**
+ * check if given $haystack string ends with $needle string
+ *
+ * @param string $haystack
+ * @param string $needle
+ * @param boolean $caseInsensitive
+ * @return boolean
+ */
+function str_ends_with(string $haystack, string $needle, bool $caseInsensitive = false)
+{
+    $length    = strlen($needle);
+    $subString = substr($haystack, strlen($haystack) - $length);
+
+    return str_contains($subString, $needle, $caseInsensitive);
+}
+
+/**
+ * limit the $string by $limit length and suffix it with the given $suffix
+ *
+ * @param string $string
+ * @param integer $limit
+ * @param string $suffix
+ * @return string
+ */
+function str_limit(string $string, int $limit, string $suffix = '...')
+{
+    return substr($string, 0, $limit) . $suffix;
+}
+
+/**
+ * get the string after $needle from the $haystack
+ *
+ * @param [type] $haystack
+ * @param [type] $needle
+ * @param boolean $caseInsensitive
+ * @return string
+ */
+function str_after(string $haystack, string $needle, bool $caseInsensitive = false)
+{
+    $position = strpos($haystack, $needle);
+
+    if ($caseInsensitive) {
+        $position = stripos($haystack, $needle);
+    }
+
+    return substr($haystack, $position + 1);
+}
+
+/**
+ * get the string before $needle from the $haystack
+ *
+ * @param string $haystack
+ * @param string $needle
+ * @param boolean $caseInsensitive
+ * @return string
+ */
+function str_before(string $haystack, string $needle, bool $caseInsensitive = false)
+{
+    $position = strpos($haystack, $needle);
+
+    if ($caseInsensitive) {
+        $position = stripos($haystack, $needle);
+    }
+
+    return substr($haystack, 0, $position);
+}
+
+/**
+ * get the string between $from and $to in the given $haystack string
+ *
+ * @param string $haystack
+ * @param string $from
+ * @param string $to
+ * @param boolean $caseInsensitive
+ * @return void
+ */
+function str_between(string $haystack, string $from, string $to, bool $caseInsensitive = false)
+{
+    $string = str_after($haystack, $from, $caseInsensitive);
+
+    return str_before($string, $to, $caseInsensitive);
+}
+
+/**
+ * strip the given string of the special chars and replace it with $replaceWith
+ *
+ * @param string $string
+ * @param string $replaceWith
+ * @return string
+ */
+function str_strip_special_chars(string $string, string $replaceWith = '')
+{
+    return preg_replace('/[^a-zA-Z0-9]/', $replaceWith, $string);
+}
+
+/**
+ * convert the given string to camel case string and strip special chars from it.
+ *
+ * @param string $string
+ * @return string
+ */
+function str_camel_case(string $string)
+{
+    $string = str_studly_case($string);
+    $string = lcfirst($string);
+
+    return str_replace(' ', '', $string);
+}
+
+function str_studly_case(string $string)
+{
+    $string = str_strip_special_chars($string, ' ');
+    $string = strtolower($string);
+    $string = ucwords($string);
+
+    return str_replace(' ', '', $string);
+}
+
+/**
+ * convert the given string to kebab case string and strip special chars from it.
+ *
+ * @param string $string
+ * @return string
+ */
+function str_kebab_case(string $string)
+{
+    $string = str_strip_special_chars($string, ' ');
+    $string = preg_replace('/([a-z0-9])?([A-Z])/', '$1 $2', $string);
+    $string = preg_replace('/(\s)+/', '-', $string);
+    return strtolower($string);
+}
+
+/**
+ * convert the given string to snake case string and strip special chars from it.
+ *
+ * @param string $string
+ * @return string
+ */
+function str_snake_case(string $string)
+{
+    $string = str_kebab_case($string);
+    return str_replace('-', '_', $string);
+}
+
+/**
+ * strip the given string of the special chars and replace it with $replaceWith
+ *
+ * @param string $string
+ * @return string
+ */
+function str_strip_numbers(string $string, string $replaceWith = '')
+{
+    return preg_replace('/[0-9]/', $replaceWith, $string);
+}
+
+/**
+ * create an array of string by breaking the given $string at $length
+ *
+ * @param string $string
+ * @param integer $length
+ * @return array
+ */
+function str_chunk(string $string, int $length = 100)
+{
+    $results = [];
+    $chunk = (strlen($string) / $length);
+    $chunk = ceil($chunk);
+    $start = 0;
+    for ($i = 0; $i < $chunk; $i++) { 
+        $results[] = substr($string, $start, $length);
+        $start    += $length;
+    }
+
+    return $results;
+}
---
+++ tests/StringTest.php
@@ -0,0 +1,125 @@
+<?php
+
+use PHPUnit\Framework\TestCase;
+use function Elphis\Helpers\Str\str_after;
+use function Elphis\Helpers\Str\str_chunk;
+use function Elphis\Helpers\Str\str_before;
+use function Elphis\Helpers\Str\str_equals;
+use function Elphis\Helpers\Str\str_between;
+use function Elphis\Helpers\Str\str_contains;
+use function Elphis\Helpers\Str\str_ends_with;
+use function Elphis\Helpers\Str\str_camel_case;
+use function Elphis\Helpers\Str\str_kebab_case;
+use function Elphis\Helpers\Str\str_snake_case;
+use function Elphis\Helpers\Str\str_starts_with;
+use function Elphis\Helpers\Str\str_studly_case;
+use function Elphis\Helpers\Str\str_strip_numbers;
+use function Elphis\Helpers\Str\str_strip_special_chars;
+
+class StringTest extends TestCase
+{
+    protected $string = 'Amidst this corona pandemic I am creating an open source project';
+
+    public function testStrEquals()
+    {
+        $string = 'amidst this corona pandemic i am creating an open source PROJECT';
+        $this->assertTrue(str_equals($this->string, $this->string));
+        $this->assertTrue(str_equals($this->string, $string, true));
+    }
+
+    public function testStrContains()
+    {
+        $this->assertTrue(str_contains($this->string, 'amidst', true));
+        $this->assertFalse(str_contains($this->string, 'amidst'));
+        $this->assertTrue(str_contains($this->string, 'Amidst'));
+        $this->assertFalse(str_contains($this->string, 'misho', true));
+        $this->assertFalse(str_contains($this->string, 'misho'));
+    }
+
+    public function testStrStartsWith()
+    {
+        $this->assertTrue(str_starts_with($this->string, 'Amidst'));
+        $this->assertTrue(str_starts_with($this->string, 'amidst', true));
+        $this->assertFalse(str_starts_with($this->string, 'project', true));
+        $this->assertFalse(str_starts_with($this->string, 'project'));
+    }
+
+    public function testStrEndsWith()
+    {
+        $this->assertTrue(str_ends_with($this->string, 'project'));
+        $this->assertTrue(str_ends_with($this->string, 'PROJECT', true));
+        $this->assertFalse(str_ends_with($this->string, 'this', true));
+        $this->assertFalse(str_ends_with($this->string, 'This'));
+    }
+
+    public function testStrAfter()
+    {
+        $after    = str_after($this->string, 'I');
+        $expected = ' am creating an open source project';
+        $this->assertEquals($after, $expected);
+    }
+
+    public function testStrBefore()
+    {
+        $after    = str_before($this->string, 'I');
+        $expected = 'Amidst this corona pandemic ';
+        $this->assertEquals($after, $expected);
+    }
+
+    public function testStrBetween()
+    {
+        $after    = str_between($this->string, 'I', 'project');
+        $expected = ' am creating an open source ';
+        $this->assertEquals($after, $expected);
+    }
+
+    public function testCamelCase()
+    {
+        $string1 = 'function-name+new_name.another%name#with$special(chars+CAPital+1+2)2(2)';
+        $result  = str_camel_case($string1);
+        $this->assertEquals($result, 'functionNameNewNameAnotherNameWithSpecialCharsCapital1222');
+    }
+
+    public function testStudlyCase()
+    {
+        $string1 = 'function-name+new_name.another%name#with$special(chars+CAPital+1+2)2(2)';
+        $result  = str_studly_case($string1);
+        $this->assertEquals($result, 'FunctionNameNewNameAnotherNameWithSpecialCharsCapital1222');
+    }
+
+    public function testKebabCase()
+    {
+        $string1 = 'function-name+new_name.another%name#with$special(chars+CAPital1-2_3*5saudJQureshi';
+        $result  = str_kebab_case($string1);
+        $this->assertEquals($result, 'function-name-new-name-another-name-with-special-chars-c-a-pital1-2-3-5saud-j-qureshi');
+    }
+
+    public function testSnakeCase()
+    {
+        $string1 = 'function-name+new_name.another%name#with$special(chars+CAPital1-2_3*5saudQureshi';
+        $result  = str_snake_case($string1);
+        $this->assertEquals($result, 'function_name_new_name_another_name_with_special_chars_c_a_pital1_2_3_5saud_qureshi');
+    }
+
+    public function testStripNumbers()
+    {
+        $string1 = 'Th!s 1 !s 4 people the wh0 type l!ke di$.';
+        $result  = str_strip_numbers($string1);
+        $this->assertEquals($result, 'Th!s  !s  people the wh type l!ke di$.');
+    }
+
+    public function testStripSpecialChars()
+    {
+        $string = 'T#!$ 0n£ !$ f0r p£oPl£ w#0 typ£ l!k£ this.';
+        $string = str_strip_special_chars($string);
+
+        $this->assertEquals($string, 'T0nf0rpoPlw0typlkthis');
+    }
+
+    public function testStringChunk()
+    {
+        $chunks = str_chunk($this->string, 32);
+        $length = ceil(strlen($this->string)/32);
+        $this->assertCount($length, $chunks);
+    }
+}
